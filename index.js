227c1229b0c7bc13c4b320a7a1a4820a84d22ff1
Akhil Karrothu <akhilkarrothu@Akhils-MacBook-Pro.local>
2020-04-23 21:55:15 -0400

    Pages done commit

--- index.js
+++ index.js
@@ -8,24 +8,24 @@ const app = new express()
 
 app.use(express.static('public'))
 
-app.use(expressEdge)
+app.use(expressEdge.engine)
 
-app.set('views' `${__dirname}/views`)
+app.set('views', `${__dirname}/views`)
 
 app.get('/', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+	res.render('index')
 })
 
 app.get('/about', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+	res.render('about')
 })
 
 app.get('/post', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+	res.render('post')
 })
 
 app.get('/contact', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+	res.render('contact')
 })
 
 app.listen(4000, () => {
